8b8b6c50fb32c99aa8168f7d58c7d1bd76bcba8d
TychaK <kelvinkibugi@gmail.com>
2023-05-28 16:30:20 +0300

    Added Supporting application routes.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Suspense} from "react";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import ArticleCategories from "./components/pages/ArticleCategories";
+import Article from "./components/pages/Article";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import Account from "./components/pages/Account";
+
+const ProtectedRoute = React.lazy(
+    () => import('./utils/ProtectedRoute')
+);
+
+const AuthenticatedRoute = React.lazy(
+    () => import('./utils/AuthenticatedRoute')
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar/>
+                <Suspense fallback={<p> Loading ... </p>}>
+                    <Routes>
+                        <Route path={'/category/:category_id'} element={<ArticleCategories/>}></Route>
+                        <Route path={'/articles'} element={<Article/>}></Route>
+                        <Route path={'/articles/:article_id'} element={<Article/>}></Route>
+                        <Route path={'/auth'} element={<AuthenticatedRoute><Login/></AuthenticatedRoute>}></Route>
+                        <Route path={'/signup'} element={<AuthenticatedRoute><Register/></AuthenticatedRoute>}></Route>
+                        <Route path={'/account'} element={<ProtectedRoute><Account/></ProtectedRoute>}></Route>
+                        <Route path={'/'} element={<Home/>}></Route>
+                    </Routes>
+                </Suspense>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
