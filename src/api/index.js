e63e49ede8aab09c314fe64c0a254cd03d427ef3
TychaK <kelvinkibugi@gmail.com>
2023-05-30 15:20:11 +0300

    Added sources store

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,12 @@ import articles from './articles.api'
 import users from './users.api'
 import categories from './categories.api'
 import authors from './authors.api'
+import sources from './sources.api'
 
 export default {
     ...articles,
     ...users,
     ...categories,
-    ...authors
+    ...authors,
+    ...sources
 }
